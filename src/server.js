63045490a1438877cd6ceb4ef7d06e7ea9aa1836
paramkr2 <param71kr@gmail.com>
2024-03-18 21:32:32 +0530

    host and port moved to environment file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import app from './index.js'
 import dbConnect from './config/db.js'
-const port = 8000 
-const server = app.listen( port , ()=>{
-	console.log(`Server running at port ${port}`)
+const PORT = process.env.PORT || 3000 
+const HOST = process.env.HOST|| 'localhost';
+const server = app.listen( PORT , HOST, ()=>{
+	console.log(`Server running at port ${PORT}`)
 })
 // listen to port 
 
